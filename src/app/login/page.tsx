4f8d1f310511743ff94fe9d44681d18a1c916408
FonzyLima <alfonsogvlima@gmail.com>
2024-03-20 00:49:47 +0800

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,30 +1,27 @@
 "use client";
 import React from "react";
+import Link from "next/link";
 import { useState } from "react";
 import axios from "axios";
-export default function Register() {
+export default function Login() {
   const [formData, setFormData] = useState({
     fName: "",
     lName: "",
     pass: "",
-    confPass: "",
   });
-
-  const registerUser = (
+  const loginUser = (
     fName: string,
     lName: string,
     pass: string,
-    confPass: string
   ) => {
     const registrationData = {
       fName: fName,
       lName: lName,
       pass: pass,
-      confPass: confPass,
     };
 
     axios
-      .post("http://localhost:8080/api/registration", registrationData)
+      .post("http://localhost:8080/api/login", registrationData)
       .then((response) => console.log(response.data)) // Handle success response
       .catch((error) => console.error("Error:", error.response.data)); // Handle error response
   };
@@ -33,11 +30,10 @@ export default function Register() {
     event.preventDefault(); // Prevent default form submission behavior
 
     // Call registerUser function with form data
-    registerUser(
+    loginUser(
       formData.fName,
       formData.lName,
       formData.pass,
-      formData.confPass
     );
   };
 
@@ -50,12 +46,9 @@ export default function Register() {
   };
 
   return (
-    <div className="bg-[#1D3461] px-[190px] py-[47px]">
-        <div className='text-center text-white text-[32px] font-bold'>Register</div>
-      <form
-        className="min-h-screen flex flex-col gap-[6px]"
-        onSubmit={handleSubmit}
-      >
+    <div className="bg-[#1D3461] px-[190px] py-[47px] min-h-screen">
+      <div className="text-center text-white text-[32px] font-bold">Login</div>
+      <form className="flex flex-col gap-[6px]" onSubmit={handleSubmit}>
         <label className="text-white">First Name:</label>
         <input
           className="rounded-[15px] px-2 py-2"
@@ -80,19 +73,15 @@ export default function Register() {
           value={formData.pass}
           onChange={handleChange}
         />
-        <label className="text-white">Confirm Password:</label>
-        <input
-          className="rounded-[15px] px-2 py-2"
-          type="password"
-          name="confPass"
-          value={formData.confPass}
-          onChange={handleChange}
-        />
         <input
           className="mt-6 w-fit rounded-[20px] px-[40px] bg-slate-500"
           type="submit"
+          value="Login"
         />
       </form>
+      <div className="mt-6 w-fit rounded-[20px] px-[40px] bg-slate-500">
+        Register
+      </div>
     </div>
   );
 }
