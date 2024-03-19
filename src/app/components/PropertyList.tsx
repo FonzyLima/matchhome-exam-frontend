34e1505f20b6489acdab64c78a40ed3f19fa4bd9
FonzyLima <alfonsogvlima@gmail.com>
2024-03-20 01:09:46 +0800

    Update PropertyList.tsx

--- src/app/components/PropertyList.tsx
+++ src/app/components/PropertyList.tsx
@@ -1,7 +1,7 @@
 import PropertyCard from "./PropertyCard";
 const PropertyList = () => {
   return (
-    <div className="min-h-screen bg-[#1D3461] flex flex-col px-[190px] py-[47px] gap-[34px]">
+    <div className="min-h-screen bg-[#1D3461] flex flex-col md:px-[250px] px-[190px] py-[47px] gap-[34px]">
       <PropertyCard propType="Amaia Laguna" model="Amaia Lands" location="Laguna" area="50sqm" price="2.2 M" availability="Ready for Ocupancy"/>
       <PropertyCard propType="Amaia Laguna" model="Amaia Lands" location="Laguna" area="50sqm" price="2.2 M" availability="Ready for Ocupancy"/>
       <PropertyCard propType="Amaia Laguna" model="Amaia Lands" location="Laguna" area="50sqm" price="2.2 M" availability="Ready for Ocupancy"/>
